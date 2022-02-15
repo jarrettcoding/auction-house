227bc2eb148db289eaddfb8be1bfd24a5bb10cf9
yasir19 <yasir.habboo@gmail.com>
2022-02-15 17:59:58 -0600

    create a session on the back end

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const routes = require('./controllers');
+const session = require('express-session');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = { 
+    secret: process.env.secret,
+    cookie: {},
+    resave: false,
+    saveUnitialize: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session(sess))
 
 app.use(routes);
 
