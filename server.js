024728a5db53e20896e5d58b87f9a438cceb39fc
Nick Garza <thenickgarza14@gmail.com>
2022-02-15 11:00:07 -0600

    set up structure for the controllers folder. Imported modles into its dedicated folder as well as set the link to begin with /api in the index.js in root of controller folder

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(routes);
+app.use(require('./controllers'));
 
 //need the routes file 
 
