e13af1cf63e8685a3667577da14f7a20e92317cc
yasir19 <yasir.habboo@gmail.com>
2022-02-15 20:22:07 -0600

    endpoint to get single and all categories

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,4 +1,54 @@
 const router = require('express').Router();
-const { Category } = require('../../models/Category');
+const { Product } = require('../../models');
+const { Category } = require('../../models');
 
+router.get('/',(req,res)=>{
+    Category.findAll({
+        attributes:[
+            'id',
+            'category_name',
+        ],
+        include: [
+            {
+                model: Product,
+                attributes:['id','product_name','price','stock','category_id']
+            }
+        ]
+    })
+    .then(catData => res.json(catData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    Category.findOne({
+      where:{
+        id: req.params.id
+      },
+      attributes:[
+        'id',
+        'category_name',
+      ],
+      // include Products association
+      include:[
+        {
+          model:Product,
+          attributes:['id','product_name','price','stock','category_id']
+        }
+      ]
+    })
+    .then(dbCatData => {
+      if(!dbCatData) {
+        res.status(400).json({message: 'The Category was no found with your rquest'});
+        return
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 module.exports = router;
