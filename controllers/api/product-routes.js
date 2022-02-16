d8521c6eb87d3013a075ec07f7b5c5c7a4587b1b
Nick Garza <thenickgarza14@gmail.com>
2022-02-15 20:12:21 -0600

    have the GET all working for getting a single product

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -1,4 +1,42 @@
 const router = require('express').Router();
-const { Product } = require('../../models/Product');
+const { Product, Category } = require('../../models');
+
+// GET /api/products
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    Product.findAll()
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err);
+    });
+});
+
+// GET /api/products/:1d
+router.get('/:id', (req, res) => {
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Category,
+                attributes: ['category_name']
+            }
+        ]
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No user found with this id!'});
+            return;
+        }
+        res.json(dbProductData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
