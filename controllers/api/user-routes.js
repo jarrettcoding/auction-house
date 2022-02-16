1245863c9307bdefa5a56064617ac976b80287e5
yohans14 <yohans14@yahoo.com>
2022-02-16 17:29:12 -0600

    crated login routes and hashed user password for authentication

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,10 +48,33 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+//login route
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
 
 router.put("/:id", (req, res) => {
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
