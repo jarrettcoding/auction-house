6c1df0185b62f2378ec05ef706b38127923787f7
Nick Garza <thenickgarza14@gmail.com>
2022-02-16 13:41:54 -0600

    added template css and also wokring on getting products to show up on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const { Product } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Product.findAll({
+        attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'description',
+            'image',
+            'category_id'
+        ],
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const product = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { 
+          product,
+         loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
