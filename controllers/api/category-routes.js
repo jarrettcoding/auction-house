b76ada88f16e428f2660905907b0323d61a9fb74
yasir19 <yasir.habboo@gmail.com>
2022-02-15 20:34:00 -0600

    endpoint for post, update and delete category

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -51,4 +51,57 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     });
   });
+  router.post('/', (req, res) => {
+    // create a new category
+    Category.create({
+      category_name: req.body.category_name,
+    })
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    })
+  });
+  router.put('/:id', (req, res) => {
+    // update a category by its `id` value
+    Category.update(req.body,{
+      category_name: req.body.category_name,
+      where:{
+        id: req.params.id
+      }
+    })
+    .then(dbCatData=>{
+      if(!dbCatData[0]) { 
+        res.status(404).json({messsage: 'Category was not found with your request'});
+      return
+      }
+      res.json(dbCatData);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json(err);
+  
+    });
+  
+  });
+
+router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
+    Category.destroy({
+      where: { 
+        id: req.params.id
+      }
+    })
+    .then(dbCatData => {
+      if(!dbCatData){
+        res.status(404).json({message: 'Category was not found with your request'})
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  });
 module.exports = router;
