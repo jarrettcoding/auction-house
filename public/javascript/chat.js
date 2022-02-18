1c859baf8d1f54e096bc885e81c9f0075761f14d
veritascoding <jarrettcoding@gmail.com>
2022-02-17 21:00:51 -0600

    basic message creation in HTML

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -6,6 +6,8 @@ const socket = io();
 socket.on('message', message => {
     console.log(message);
     outputMessage(message); 
+
+    // scroll down
 });
 
 // message submit
@@ -15,11 +17,17 @@ chatForm.addEventListener('submit', (e) => {
     // get message text
     const msg = e.target.elements.msg.value; 
 
-    // 
+    // emit message to server
     socket.emit('chatMessage', msg); 
 }); 
 
 // output message to DOM
 function outputMessage(message) { 
     const div = document.createElement('div')
+    div.classList.add('message'); 
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`; 
+    document.querySelector('.chat-messages').appendChild(div); 
 }; 
