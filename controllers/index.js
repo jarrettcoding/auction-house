b40f97f8c20ea1fec6993cfe479fac67cb402b32
yasir19 <yasir.habboo@gmail.com>
2022-02-18 21:27:28 -0600

    getting all the update and fixing the conflict

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,15 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
-const dashboardRoutes = require("./dashboard-routes.js");
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const singleProduct = require('./single-product');
 
 // sets the link to be localhost/api/then routes
-router.use("/api", apiRoutes);
-router.use("/", homeRoutes);
-router.use("/dashboard", dashboardRoutes);
-
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/product',singleProduct)
 // Must be at the botton (LEARNED THE HARD WAY)
 router.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 module.exports = router;
