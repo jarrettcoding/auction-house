00ec77f1ede35f409c04d1c8882c1506fde0677d
yohans14 <yohans14@yahoo.com>
2022-02-18 20:48:42 -0600

    fixed the bug with signup

--- models/Category.js
+++ models/Category.js
@@ -1,28 +1,28 @@
-const { Model, DataTypes } = require('sequelize'); 
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Category extends Model {}
 
 Category.init(
-    {
-        id: {
-            type: DataTypes.INTEGER, 
-            primaryKey: true, 
-            autoIncrement: true, 
-            allowNull: false
-        }, 
-        category_name: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        }
-    }, 
-    {
-        sequelize, 
-        timestamps: false, 
-        freezeTableName: true, 
-        underscored: false, 
-        modelName: 'category'
-    }
-); 
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: false,
+    modelName: "category",
+  }
+);
 
-module.exports = Category; 
+module.exports = Category;
