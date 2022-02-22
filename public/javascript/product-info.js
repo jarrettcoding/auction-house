1290bb1b1e6bb0bcb1e70095cacbc5f8714e19ae
yasir19 <yasir.habboo@gmail.com>
2022-02-22 10:43:09 -0600

    update the the homepage and dashboard routes

--- public/javascript/product-info.js
+++ public/javascript/product-info.js
@@ -19,31 +19,29 @@ getInfo = (data) => {
 
   const date = data.map(({category}) => category);
 
-  const dateArr = date.map(({id, event_date})=({
-    name: event_date,
+  const dateArr = date.map(({id, event_time})=>({
+    name: event_time,
     value:id,
   }));
+  newDate = dateArr.map(({name})=>name)
 
+  // var today = new Date();
+  // var newDate = today.getFullYear()+"-"+(today.getMonth()+1)+'-'+today.getDate();
+  // console.log(date);
+  // if(newDate== date){
 
-
-  
-
-
-
-
-setInterval(() => {
-  console.log(dateArr
-    )
- console.log('data',data);
+ console.log('data',newDate);
   const result = imagArr.map(({ name }) => name);
   for (let i = 0; i < result.length; i++) {
     let imgEl = document.createElement("img");
     imgEl.setAttribute("class", "thumbnail");
     imgEl.src = `/images/${result[i]}`;
-    let imageEl = document.getElementById("preview");
+    let imageEl = document.querySelector(".item");
     imageEl.appendChild(imgEl);
   }
-}, 2000);
 };
+  // }else {
+  //   alert('nowproduct')
+  // }
 
 getImage();
