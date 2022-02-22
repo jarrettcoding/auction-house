957928f3ff0218269aadb864f9f5c65ac8da49aa
yasir19 <yasir.habboo@gmail.com>
2022-02-21 19:19:50 -0600

    working on date

--- public/javascript/product-info.js
+++ public/javascript/product-info.js
@@ -2,36 +2,48 @@ function getImage() {
   fetch(`/api/products`).then((res) => {
     if (res.ok) {
       res.json().then((data) => {
-          imgArr(data);
+          getInfo(data);
       });
     } else {
       alert(res.statusText);
     }
   });
-  imgArr();
 }
+// get values 
+getInfo = (data) => {
 
-
-imgArr = (data) => {
-  const imArr = data.map(({image, id }) => ({
+  const imagArr = data.map(({image, id }) => ({
     name: image,
     value: id,
   }));
 
+  const date = data.map(({category}) => category);
 
-  setInterval(() => {
-    console.log(date)
+  const dateArr = date.map(({id, event_date})=({
+    name: event_date,
+    value:id,
+  }));
+
+
+
   
-    const result = imArr.map(({ name }) => name);
-    for (let i = 0; i < result.length; i++) {
-      let imgEl = document.createElement("img");
-      imgEl.setAttribute("class", "thumbnail");
-      imgEl.src = `/images/${result[i]}`;
-      let imageEl = document.getElementById("preview");
-      imageEl.appendChild(imgEl);
-    }
-  }, 2000);
-};
 
+
+
+
+setInterval(() => {
+  console.log(dateArr
+    )
+ console.log('data',data);
+  const result = imagArr.map(({ name }) => name);
+  for (let i = 0; i < result.length; i++) {
+    let imgEl = document.createElement("img");
+    imgEl.setAttribute("class", "thumbnail");
+    imgEl.src = `/images/${result[i]}`;
+    let imageEl = document.getElementById("preview");
+    imageEl.appendChild(imgEl);
+  }
+}, 2000);
+};
 
 getImage();
