65f01546767c811c1ede0f369c8e8a7262121841
yasir19 <yasir.habboo@gmail.com>
2022-02-23 08:15:48 -0600

    refactore some of the code

--- public/javascript/bid.js
+++ public/javascript/bid.js
@@ -1,11 +1,8 @@
 async function bidFormHandler(event) {
     event.preventDefault();
-
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-  
-
     const res = await fetch(`/api/products/${id}`,) 
     .then((res)=>{
         if(res.ok){
@@ -21,14 +18,14 @@ async function bidFormHandler(event) {
     let oldPrice = data.price
         bid(oldPrice);
     }
+
 }
-
-  
-
 async function bid(data) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+
+    
     const price = document.querySelector('#bid').value.trim();
     let oldPrice = data;
     console.log(oldPrice);
@@ -53,3 +50,4 @@ async function bid(data) {
   }
 }
   document.querySelector('.bid-form').addEventListener('submit', bidFormHandler);
+
