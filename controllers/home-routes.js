62b70462e7e49788dc349686f0c579f87450644d
yasir19 <yasir.habboo@gmail.com>
2022-02-23 23:39:35 -0600

    fix homeroute

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -44,25 +44,4 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get('/',(req,res)=>{
-  Category.findAll({
-      attributes:[
-          'id',
-          'category_name',
-          'event_time',
-      ],
-      include: [
-          {
-              model: Product,
-              attributes:['id','product_name','price','stock','category_id']
-          }
-      ]
-  })
-  .then(catData => res.json(catData))
-  .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-  })
-});
-
 module.exports = router;
